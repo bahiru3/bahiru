e44d02a04266b8d53544951518f2e13a6fe3525e
bahiru3 <bahirumegersa681@gmail.com>
2023-12-18 10:03:01 +0300

    FFG

---
+++ js/jsvalidation.js
@@ -0,0 +1,23 @@
+let fullName={
+firstName:'bahiru',
+lastNmae:'Megersa'
+
+}
+    alert.fullName(fullName.firstName);
+    
+    
+    let persondetails{
+        let fullName:{
+            firstName:'bahiru',
+            lastNmae:'Megersa'
+            
+            },
+            alertMessge:funcation(messge){
+                alert(messge);
+
+
+                        }}
+
+            persondetails.alertMessge()
+
+    
