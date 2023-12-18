8fb491d02c9b86d5e02c0417a6512d1f495881f2
bahiru3 <bahirumegersa681@gmail.com>
2023-12-18 13:48:51 +0300

    o

--- js/index.php
+++ js/index.php
@@ -1,4 +1,11 @@
 <?php
-echo'hello World';
+ 
+$name = "Krishna";
+$name1 = "Bahireu";
+echo "The name of the Proffessional is $name \n";
+echo 'The name of the Proffessional is $name1 ';
+echo "\n\n";
+ 
+//returns data type, size and value
+var_dump($name,
 ?>
-
